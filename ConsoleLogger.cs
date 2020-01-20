9d4ff63f8f4ef0cb213884ae2817ab7ca4da8250
makakoocelot <makakoocelot@gmail.com>
2020-01-20 10:55:23 +0100

    Create the ilogger interface and implement it in the consolelogger class

---
+++ ConsoleLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessNote
+{
+    class ConsoleLogger:Ilogger
+    {
+        public void Info(string message)
+        {
+            string info = "INFO ";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(info);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(message);
+        }
+
+        public void Error(string message)
+        {
+            string error = "Error ";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(error);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Ilogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessNote
+{
+    interface Ilogger
+    {
+        public void Info(string message);
+
+        public void Error(string message);
+    }
+}
